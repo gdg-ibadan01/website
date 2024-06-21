0fc00abdf752e66321bb0721b4a71a9344a80e22
abdulroheem <ayanwunmiabdulroheem@gmail.com>
2024-06-21 23:11:53 +0100

    fix: our mission section responsiveness done

--- components/Homepage/Mission/index.vue
+++ components/Homepage/Mission/index.vue
@@ -9,13 +9,18 @@
 
 			<div class="flex justify-between flex-wrap">
 				<div
-					class="lg:w-[433px] w-[100%] lg:h-[800px] h-[750px] py-[30px] rounded-[32px] bg-blue-500 px-0 lg:px-[27px] lg:py-[17px] relative mb-[28px] lg:mb-[0] flex flex-col-reverse lg:flex-col">
-					<div class="relative w-[100%] h-[500px]">
-						<img src="~/assets/images/overlay-img-3.png" alt="" class="absolute z-30 bottom-[0px]">
-						<img src="~/assets/images/overlay-img-2.png" alt=""
-							class="absolute z-20 bottom-0 lg:bottom-[50px] right-[30px] w-[204px] h-[390px] object-cover">
-						<img src="~/assets/images/overlay-img-1.png" alt=""
-							class="absolute top-0 lg:top-[-50px] right-[15px] lg:right-0 z-10">
+					class="lg:w-[433px] w-[100%] lg:h-[800px] h-[750px] py-[60px] rounded-[32px] bg-blue-500 px-0 lg:px-[27px] lg:py-[17px] relative mb-[28px] lg:mb-[0] flex flex-col-reverse lg:flex-col">
+					<div class="relative w-auto h-[375px]">
+						<div class="lg:w-[243] lg:h-[100%] z-10 absolute lg:right-0 right-[-20px] w-[254px] h-[307px]">
+							<img src="~/assets/images/overlay-img-1.png" alt="" class="w-full h-full object-contain">
+						</div>
+						<div class="lg:w-auto lg:h-[100%] w-[304px] h-[460px] z-20 absolute lg:bottom-[-70px] bottom-[-80px] lg:right-0 right-4">
+							<img src="~/assets/images/overlay-img-2.png" alt="" class="w-full h-full object-contain">
+						</div>
+						<div
+							class="lg:w-[451px] lg:h-[100%] z-30 absolute lg:right-[-40px] right-0 lg:bottom-[-140px] bottom-[-70px] w-[344px] h-[340px]">
+							<img src="~/assets/images/overlay-img-3.png" alt="" class="w-full h-full object-contain">
+						</div>
 					</div>
 					<div
 						class="lg:absolute static lg:bottom-[82px] bottom-0 max-w-[339px] px-[20px] lg:px-0 mb-5 lg:mb-0">
@@ -30,7 +35,8 @@
 					</div>
 				</div>
 				<div class="w-[711px] h='100% flex flex-col justify-between items-center">
-					<div class="bg-orange-500 py-[36px] px-[30px] relative w-[100%] h-[402px] lg:h-auto rounded-[32px] mb-[48px]">
+					<div
+						class="bg-orange-500 py-[36px] px-[30px] relative w-[100%] h-[402px] lg:h-auto rounded-[32px] mb-[48px]">
 						<div
 							class="absolute lg:right-[10px] right-[10px] lg:top-[-100px] bottom-[0] w-[186px] lg:w-[352px] lg:h-[393px]">
 							<img src="~/assets/images/review-image.png" alt="" class="w-[100%] object-cover h-[100%]">
@@ -55,7 +61,8 @@
 					</div>
 					<div class="bg-green-500 py-5 lg:h-[360px] h-[440px] px-5 w-[100%] rounded-[32px] flex relative">
 						<div class="flex flex-col lg:justify-end justify-start">
-							<div class="absolute lg:top-[-50px] bottom-[10px] lg:w-[210px] lg:h-[244px] w-[150px] h-[156px] z-30 lg:z-0">
+							<div
+								class="absolute lg:top-[-50px] bottom-[10px] lg:w-[210px] lg:h-[244px] w-[150px] h-[156px] z-30 lg:z-0">
 								<img src="~/assets/images/build.png" alt="" class="w-full h-full object-cover">
 							</div>
 
@@ -71,10 +78,12 @@
 							</div>
 						</div>
 						<div class="absolute top-0 lg:right-[-70px] right-[0px]">
-							<div class="relative lg:top-[-50px] top-[150px] right-[100px] lg:w-[233px] lg:h-[244px] w-[201px] h-[208px]">
+							<div
+								class="relative lg:top-[-50px] top-[150px] right-[100px] lg:w-[233px] lg:h-[244px] w-[201px] h-[208px]">
 								<img src="~/assets/images/members.png" alt="" class="w-full h-full object-cover">
 							</div>
-							<div class="relative lg:bottom-[150px] bottom-[0px] z-20 lg:w-[370px] lg:h-[365px] w-[238px] h-[236px]">
+							<div
+								class="relative lg:bottom-[150px] bottom-[0px] z-20 lg:w-[370px] lg:h-[365px] w-[238px] h-[236px]">
 								<img src="~/assets/images/review.png" alt="" class="object-cover w-full h-full">
 							</div>
 						</div>
