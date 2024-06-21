3e0a923b4c2de9a348d6079c10901d6ad323bafe
abdulroheem <ayanwunmiabdulroheem@gmail.com>
2024-06-21 19:00:55 +0100

    fix: our mission section responsiveness done

--- components/Homepage/Mission/index.vue
+++ components/Homepage/Mission/index.vue
@@ -9,28 +9,16 @@
 
 			<div class="flex justify-between flex-wrap">
 				<div
-					class="lg:w-[433px] w-[100%] lg:h-[800px] h-[750px] py-[30px] rounded-[32px] bg-blue-500 px-0 lg:px-[27px] lg:py-[17px] relative mb-[28px] lg:mb-[0] flex flex-col-reverse lg:flex-col"
-				>
+					class="lg:w-[433px] w-[100%] lg:h-[800px] h-[750px] py-[30px] rounded-[32px] bg-blue-500 px-0 lg:px-[27px] lg:py-[17px] relative mb-[28px] lg:mb-[0] flex flex-col-reverse lg:flex-col">
 					<div class="relative w-[100%] h-[500px]">
-						<img
-							src="~/assets/images/overlay-img-3.png"
-							alt=""
-							class="absolute z-30 bottom-[0px]"
-						>
-						<img
-							src="~/assets/images/overlay-img-2.png"
-							alt=""
-							class="absolute z-20 bottom-0 lg:bottom-[50px] right-[30px] w-[204px] h-[390px] object-cover"
-						>
-						<img
-							src="~/assets/images/overlay-img-1.png"
-							alt=""
-							class="absolute top-0 lg:top-[-50px] right-[15px] lg:right-0 z-10"
-						>
+						<img src="~/assets/images/overlay-img-3.png" alt="" class="absolute z-30 bottom-[0px]">
+						<img src="~/assets/images/overlay-img-2.png" alt=""
+							class="absolute z-20 bottom-0 lg:bottom-[50px] right-[30px] w-[204px] h-[390px] object-cover">
+						<img src="~/assets/images/overlay-img-1.png" alt=""
+							class="absolute top-0 lg:top-[-50px] right-[15px] lg:right-0 z-10">
 					</div>
 					<div
-						class="lg:absolute static lg:bottom-[82px] bottom-0 max-w-[339px] px-[20px] lg:px-0 mb-5 lg:mb-0"
-					>
+						class="lg:absolute static lg:bottom-[82px] bottom-0 max-w-[339px] px-[20px] lg:px-0 mb-5 lg:mb-0">
 						<h2 class="text-[36px] text-white-primary mb-[18px] font-semibold">
 							About us
 						</h2>
@@ -42,24 +30,16 @@
 					</div>
 				</div>
 				<div class="w-[711px] h='100% flex flex-col justify-between items-center">
-					<div class="bg-orange-500 py-[36px] px-[30px] relative w-[100%] rounded-[32px] mb-[48px]">
+					<div class="bg-orange-500 py-[36px] px-[30px] relative w-[100%] h-[402px] lg:h-auto rounded-[32px] mb-[48px]">
 						<div
-							class="absolute lg:right-[10px] right-[10px] lg:top-[-100px] bottom-[0] w-[156px] lg:w-[352px] lg:h-[393px]"
-						>
-							<img
-								src="~/assets/images/review-image.png"
-								alt=""
-								class="w-[100%] object-cover h-[100%]"
-							>
+							class="absolute lg:right-[10px] right-[10px] lg:top-[-100px] bottom-[0] w-[186px] lg:w-[352px] lg:h-[393px]">
+							<img src="~/assets/images/review-image.png" alt="" class="w-[100%] object-cover h-[100%]">
 						</div>
 						<div class="flex lg:flex-col flex-col-reverse">
-							<div class="w-[109px] lg:w-[216px]">
-								<img
-									src="~/assets/images/map-2.png"
-									alt=""
-								>
+							<div class="w-[159px] lg:w-[216px]">
+								<img src="~/assets/images/map-2.png" alt="">
 							</div>
-							<div>
+							<div class="mb-5 lg:mb-0">
 								<h2 class="mb-[12px] text-white-primary lg:text-[36px] text-[24px] font-semibold">
 									Our Mission
 								</h2>
@@ -73,37 +53,30 @@
 							</div>
 						</div>
 					</div>
-					<div
-						class="bg-green-500 py-[36px] lg:h-[360px] h-[440px] px-[30px] relative w-[100%] rounded-[32px] lg:flex-col flex-col"
-					>
-						<div
-							class=" absolute lg:left-[30px] left-[10px] lg:top-[-50px] bottom-[-80px] w-[140px] lg:w-[210px] h-[244px]"
-						>
-							<img
-								src="~/assets/images/build.png"
-								alt=""
-							>
+					<div class="bg-green-500 py-5 lg:h-[360px] h-[440px] px-5 w-[100%] rounded-[32px] flex relative">
+						<div class="flex flex-col lg:justify-end justify-start">
+							<div class="absolute lg:top-[-50px] bottom-[10px] lg:w-[210px] lg:h-[244px] w-[150px] h-[156px] z-30 lg:z-0">
+								<img src="~/assets/images/build.png" alt="" class="w-full h-full object-cover">
+							</div>
+
+							<div class="max-w-[401px]">
+								<h2 class="mb-[12px] text-white-primary lg:text-[36px] text-[24px] font-semibold">
+									Why Join GDG
+								</h2>
+								<p class="text-white-primary lg:text-[18px] text-[16px] font-normal">
+									You'll be part of a supportive community that empowers you to explore, grow, and
+									succeed
+									in the ever-evolving tech world.
+								</p>
+							</div>
 						</div>
-						<div class="relative right-0 w-[100%] h-[100%]">
-							<img
-								src="~/assets/images/members.png"
-								alt=""
-								class="absolute z-10 top-[-80px] right-[120px] w-[255px] h-[244px] object-cover"
-							>
-							<img
-								src="~/assets/images/review.png"
-								alt=""
-								class="absolute z-20 right-[-100px] w-[370px] h-[365px] object-cover bottom-[-120px]"
-							>
-						</div>
-						<div class="max-w-[401px] lg:absolute static bottom-[35px]">
-							<h2 class="mb-[12px] text-white-primary lg:text-[36px] text-[24px] font-semibold">
-								Why Join GDG
-							</h2>
-							<p class="text-white-primary lg:text-[18px] text-[16px] font-normal">
-								You'll be part of a supportive community that empowers you to explore, grow, and succeed
-								in the ever-evolving tech world.
-							</p>
+						<div class="absolute top-0 lg:right-[-70px] right-[0px]">
+							<div class="relative lg:top-[-50px] top-[150px] right-[100px] lg:w-[233px] lg:h-[244px] w-[201px] h-[208px]">
+								<img src="~/assets/images/members.png" alt="" class="w-full h-full object-cover">
+							</div>
+							<div class="relative lg:bottom-[150px] bottom-[0px] z-20 lg:w-[370px] lg:h-[365px] w-[238px] h-[236px]">
+								<img src="~/assets/images/review.png" alt="" class="object-cover w-full h-full">
+							</div>
 						</div>
 					</div>
 				</div>
