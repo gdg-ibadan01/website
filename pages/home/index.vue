d14884f08191e838eec0fddbb44acb171531651c
Semight <davidoluwasemiloorei@gmail.com>
2024-08-25 20:04:52 +0100

    started working on the home page

--- pages/home/index.vue
+++ pages/home/index.vue
@@ -1,12 +1,20 @@
 <script setup>
-import GdgIcon from '~/components/icons/GdgIcon.vue';
+import AppHeader from '@/components/common/AppHeader';
+import CustomFooter from '@/components/common/Footer';
 </script>
 
 <template>
-	<div class="h-[100vh] w-[100vw] flex justify-center items-center">
-		<GdgIcon
-			width="500"
-			height="64"
-		/>
+	<div>
+		<div class="py-8">
+			<AppHeader />
+		</div>
+		<div class="mt-[120px]">
+			<HomepageHero />
+			<HomepageMission />
+			<HomepageWhatWeOffer />
+			<HomepageUpcomingEvents />
+			<HomepageSponsors />
+			<CustomFooter />
+		</div>
 	</div>
 </template>
